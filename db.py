6d9f07ba2ca05759b0f3fca412c1303aa72083cf
clara <10088152+claraj@users.noreply.github.com>
2020-02-26 16:48:22 -0600

    outline for functions, classes

--- db.py
+++ db.py
@@ -2,3 +2,20 @@
 save bookmark(data)
 show all bookmark()
 """
+
+# Set up database 
+
+""" first version: 4 columns:  top news story, top restaurant, some weather number, date saved.  
+
+""" 
+
+# think about table(s)
+
+def save(rest, news, weather):
+    # save these things to the database
+    pass 
+
+
+def get_all():
+    # get all things, organize into list ?  and return 
+    return []
--- weather.py
+++ weather.py
@@ -1,3 +1,40 @@
 """
 get weather for the city
 """
+
+from dataclasses import dataclass
+
+""" Think about what data to return? """
+
+def get_forecast(city):
+    
+    # make api call
+    # make Forecast objects 
+    # return list of Forecast objects
+
+    response = make_api_call(city)
+
+    list_forecasts  = process_json(response)
+
+    return list_forecasts
+
+
+def make_api_call(city):
+    # fill in here 
+    return {}   
+
+
+def process_json(json):
+
+    # use actual data from json
+    today = Forecast('sunshine', 30)
+    tomorrow = Forecast('cloudy', 23)
+    return [today, tomorrow]
+
+
+@dataclass
+class Forecast:
+    description: str 
+    temp: float
+
+
