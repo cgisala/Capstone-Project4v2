bf6856dec5fdfd8ea95aee80f266cf19c8dbe35e
chris <chris@chris-pc>
2020-03-04 10:16:34 -0600

    Added comments

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-"""
-1. call method to Displays menu
-2. def search city
-3. def view calls saved data from the database
-"""
-
 import ui 
 import search
 import db
 
-
 def main():
+
+    # Loops until the user choose quit
     while True:
-        choice = ui.show_menu()
+        choice = ui.show_menu() # Calls the show menu function from the ui module and returns the user choice from the menu.
         if choice == 'quit':
             break
 
@@ -20,16 +15,14 @@ def main():
 
         elif choice == 'show':
             show()
-
         
 def search_city():
     """ get city. make api calls. show data """
     search.search()
 
-
 def show():
     """ show all saved city data"""
-    all_data = db.get_all()
-    ui.display(all_data)
+    all_data = db.get_all() # Gets all the bookmarked data from the database
+    ui.display(all_data)  # Displays all the records from the database.
 
 
